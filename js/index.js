81ae082e32cd46ac63aaed3a20d2645a41289787
liqing <1293696719@qq.com>
2023-07-22 14:54:42 +0800

    添加功能

--- js/index.js
+++ js/index.js
@@ -13,9 +13,7 @@ function getBookList() {
       creator
     }
   }).then(result=>{
-    console.log(result)
     const bookList = result.data.data
-    console.log(bookList)
     //1.2渲染数据
     const htmlStr = bookList.map((item,index)=>{
       return `<tr>
@@ -29,10 +27,41 @@ function getBookList() {
       </td>
     </tr>`
     }).join('')
-    console.log(htmlStr);
     document.querySelector('.list').innerHTML = htmlStr
   })
 }
 
 // 网页加载运行后，获取并渲染列表一次
 getBookList()
+
+/*
+目标2：新增图书
+2.1新增弹框->显示和隐藏
+2.2收集表单数据，并提交到服务器保存
+2.3刷新图书列表
+*/ 
+
+//2.1 创建弹框对象
+const addModalDom = document.querySelector('.add-modal')
+const addModal = new bootstrap.Modal(addModalDom)
+document.querySelector('.add-btn').addEventListener('click',()=>{
+// 2.2收集表单数据，并提交到服务器保存
+const addForm = document.querySelector('.add-form')
+const bookObj = serialize(addForm,{hash:true,empty:true})
+console.log(bookObj);
+axios({
+  url:'http://hmajax.itheima.net/api/books',
+  method:'post',
+  data:{
+    ...bookObj,
+    creator
+  }
+}).then(result=>{
+  // 2.3刷新图书列表
+  getBookList()
+  // 重置表单
+  addForm.reset()
+  // 隐藏弹框
+  addModal.hide()
+})
+})
